bdf9bf009eab474e25145f39d651df07942424b3
yonnik <137620324+yonnik@users.noreply.github.com>
2024-12-31 12:02:29 +0000

    Set up card component and styled it.

---
+++ my-react-ap/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+return(
+  <div className='card'>
+    <img className = "card-image" src="./roshi.jpg" width="130" height="150" alt="profile picture" />
+    <h2>Yonndada</h2>
+    <p>ImposterGrammer</p>
+  </div>
+)
+}
+
+export default Card 
