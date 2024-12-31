30e859321da0fad767884c1d7691de0c0f87d795
yonnik <137620324+yonnik@users.noreply.github.com>
2024-12-31 14:58:22 +0000

    Fixed bugs with proptypes by installing an npm package.

--- my-react-ap/src/Students.jsx
+++ my-react-ap/src/Students.jsx
@@ -1,3 +1,6 @@
+import PropTypes from 'prop-types'
+//Prop types helps us make sure we are passing through the correct vale. Say our name prop has to be a string, age has to be a number, is? has to be a boolean. 
+
 function Student(props){
   return(
     <div className="student">
@@ -7,5 +10,10 @@ function Student(props){
     </div>
   )
 }
-
+Student.propTypes = {
+name: PropTypes.string,
+age: PropTypes.number,
+isStudent: PropTypes.bool,
+}
+//This creates the proptypes for each key5 
 export default Student
