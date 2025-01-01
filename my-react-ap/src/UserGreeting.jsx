e6634e219d4f58094aa6cb5d1528df9940606751
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-01 11:12:53 +0000

    Made the readability more clearrer by putting the h2 elements in a variable

--- my-react-ap/src/UserGreeting.jsx
+++ my-react-ap/src/UserGreeting.jsx
@@ -3,11 +3,14 @@
 
 function UserGreeting({isLoggedIn, username}){
 
+  const welcomeMessage = <h2 className="welcome-message">Welcome {username}</h2> 
+
+  const loginPrompt = <h2 className= "login-prompt">Please Log in to continue</h2>
 
 return(
-  isLoggedIn? 
-<h2 className="welcome-message">Welcome {username}</h2> : 
-<h2 className= "login-prompt">Please Log in to continue</h2>
+  
+  isLoggedIn? welcomeMessage : loginPrompt
+
 )
 
 }
