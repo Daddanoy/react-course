5137e75b50539945c46c2e26d6acae2611931267
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-02 12:36:09 +0000

    Mapped eached fruit and gave each object a key.  Added calories to each object.

--- my-react-ap/src/List.jsx
+++ my-react-ap/src/List.jsx
@@ -2,15 +2,19 @@
 
 function List(){
 
-const fruits = ['apple', 'orange', 'banana', 'coconut', 'pineapple']
+const fruits = [  {id: 1, name:'apple', calories: 95},
+                  {id: 2, name:'orange', calories:45}, 
+                  {id: 3,name:'banana', calories: 105}, 
+                  {id: 4,name:'coconut', calories: 159},
+                   {id: 5 ,name:'pineapple', calories: 37}];
 
-fruits.sort()
+//fruits.sort()
 
-const listItems = fruits.map(fruit => <li>{fruit}</li> 
+const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}</li> 
 
 )
 
-return (<ul>{listItems}</ul>)
+return (<ol>{listItems}</ol>)
 
 }
 
