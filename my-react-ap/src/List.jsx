ad9c5293efd0cf92a583938aff553db5c05875f2
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-03 11:54:36 +0000

    Filtered low cal fruit with its correct id's an properties

--- my-react-ap/src/List.jsx
+++ my-react-ap/src/List.jsx
@@ -13,12 +13,14 @@ const fruits = [  {id: 1, name:'apple', calories: 95},
 //fruits.sort((a,b)=> a.name.localeCompare(b.name) ) SORTS STINGS IN AN ARRAY BY THEIR NAME PROPERTY ALPHABETICALLY
 //fruits.sort((a,b)=> b.name.localeCompare(a.name) ) reverse ALPHABETICAL
 
-fruits.sort((a,b) => a.calories - b.calories)//NUMERIC ORDER
-fruits.sort((a,b) => b.calories - a.calories)//REVERSE NUMERIC ORDER
+//fruits.sort((a,b) => a.calories - b.calories)//NUMERIC ORDER
+//fruits.sort((a,b) => b.calories - a.calories)//REVERSE NUMERIC ORDER
 
-const listItems = fruits.map(fruit => <li key={fruit.id}>
-                                      {fruit.name}: &nbsp;
-                                      <b>{fruit.calories}</b></li> 
+const lowCalFruits = fruits.filter(fruit =>fruit.calories < 100)
+
+const listItems = lowCalFruits.map(lowCalFruit => <li key={lowCalFruit.id}>
+                                      {lowCalFruit.name}: &nbsp;
+                                      <b>{lowCalFruit.calories}</b></li> 
 
 )
 
