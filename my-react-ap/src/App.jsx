a78795eb9f8c9814280d00badf214e774efbd380
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-03 14:34:45 +0000

    Made it so it returns null if there is no data in the object

--- my-react-ap/src/App.jsx
+++ my-react-ap/src/App.jsx
@@ -13,12 +13,12 @@ const vegetables = [ {id: 6, name:'potatoes', calories: 110},
                     {id: 7, name:'celery', calories:15}, 
                      {id: 8,name:'carrots', calories: 25}, 
                     {id: 9,name:'corn', calories: 63},
-                    {id: 10 ,name:'broccoli', calories: 50}];
+                    { id: 10 ,name:'broccoli', calories: 50}];
                     
       return (
   <>
-  <List items={fruits} category='Fruits'/>
-  <List items={vegetables} category="Vegetables" />
+ {fruits.length > 0? <List items={fruits} category='Fruits'/> : null} 
+ {vegetables.length > 0 ? <List items={vegetables} category="Vegetables"/> : null}
   </>
   
 )
