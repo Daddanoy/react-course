334000f62cabaed9f6b3b66b33dfeac459a01c3d
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-09 14:27:22 +0000

    SHOWS ALL PROPERTIES OF TARGET E

--- my-react-ap/src/Buton.jsx
+++ my-react-ap/src/Buton.jsx
@@ -1,9 +1,24 @@
 
 function Button (){
-  const handleClick = () =>console.log('OUCH');
+  
+const handleClick = (e)=> console.log(e)//SHOWS ALL THE PROPERTIES LINKED TO THIS OBJECT.
+ 
 
-  const handleClick2 = (name)=> console.log(`${name} stop clicking me`)
+  return(<button onClick={(e)=> handleClick(e)}>Click Me</button>)
 
-  return(<button onClick={()=>handleClick2('Yonnik')}>Click Me</button>)//if you add a set of paraenthesis after a callback it will invoke it right away.
+    //const handleClick2 = (name)=> console.log(`${name} stop clicking me`)
+
+  //return(<button onClick={()=>handleClick2('Yonnik')}>Click Me</button>)//if you add a set of paraenthesis after a callback it will invoke it right away.
+
+   /*let count = 0
+  const handleClick = (name) =>{
+    if(count <3){
+      count++
+      console.log(`${name} clicked me ${count} times`)
+    }else {
+      console.log(`${name} stop clicking me`)
+    }
+  };
+*/
 }
 export default Button  
