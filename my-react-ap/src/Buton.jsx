ad86d06c7550388bba7931d6b49c15853b66b1d7
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-09 14:06:15 +0000

    Send arguments to a function in react

--- my-react-ap/src/Buton.jsx
+++ my-react-ap/src/Buton.jsx
@@ -4,6 +4,6 @@ function Button (){
 
   const handleClick2 = (name)=> console.log(`${name} stop clicking me`)
 
-  return(<button onClick={()=>handleClick2('Yonnik')}>Click Me</button>)//if you add a set of paraenthesis after a callback it will invoke it right away
+  return(<button onClick={()=>handleClick2('Yonnik')}>Click Me</button>)//if you add a set of paraenthesis after a callback it will invoke it right away.
 }
 export default Button  
