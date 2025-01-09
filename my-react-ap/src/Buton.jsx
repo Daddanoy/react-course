fd1d421c9dd1d90874fa7cab4dda8f8c165a9ec7
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-09 13:43:25 +0000

    Added a handle click function to print users name. Explination of parenthesis on a callback function in a return block

--- my-react-ap/src/Buton.jsx
+++ my-react-ap/src/Buton.jsx
@@ -1,9 +1,9 @@
-function Button(){
 
-  const handleClick = ()=> console.log('Ouch');
+function Button (){
+  const handleClick = () =>console.log('OUCH');
 
+  const handleClick2 = (name)=> console.log(`${name} stop clicking me`)
 
-return (<button onClick={handleClick}>Click Me 😘</button>)
+  return(<button onClick={()=>handleClick2('Yonnik')}>Click Me</button>)//if you add a set of paraenthesis after a callback it will invoke it right away
 }
-
-export default Button
+export default Button  
