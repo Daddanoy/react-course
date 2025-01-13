222dd44822ff2f36b1b113a92fc4cfbee09fca7c
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-13 08:46:22 +0000

    upodate

--- my-react-ap/src/profilepicture.jsx
+++ my-react-ap/src/profilepicture.jsx
@@ -6,4 +6,4 @@ const handleClick = (e)=> e.target.style.display = 'none'
 return (<img onClick={(e)=>handleClick(e)} src={imageUrl}></img>)
 }
 //const handleClick = (e)=> console.log('Ouch')
-export default ProfilePicture
+export default ProfilePicture  
