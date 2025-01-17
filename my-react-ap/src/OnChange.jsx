f00c9c7e5f290e7b58b567a5f4d93dfcc9da78e2
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-17 14:01:12 +0000

    ONCHANGE PAYMENT TYPES

--- my-react-ap/src/OnChange.jsx
+++ my-react-ap/src/OnChange.jsx
@@ -4,6 +4,8 @@ function OnChange (){
 
   const [name, setName] = useState("");
   const [qunatity, setQuantity] = useState();
+  const [comment, setComment] = useState("");
+  const [payment, setPayment] = useState("")
 
   function handleQuantityChange(event){
     setQuantity(event.target.value)
@@ -14,6 +16,13 @@ function OnChange (){
     setName(event.target.value)
   }
 
+  function handleCommentChange(event){
+    setComment(event.target.value)
+  }
+
+  function handlePaymentChange(event){
+    setPayment(event.target.value)
+  }
 
   return(<div>
     <input value ={name} onChange={handleChange} />
@@ -21,7 +30,20 @@ function OnChange (){
 
     <input value = {qunatity} onChange={handleQuantityChange} type='number' />
     <p> Qunatity: {qunatity}</p>
+
+    <textarea value={comment} onChange={handleCommentChange} placeholder='Enter delivery Instructions'></textarea>
+    <p>Comment: {comment}</p>
+
+    <select value={payment} onChange={handlePaymentChange}>
+      <option value =""> Select an option</option>
+      <option value="Visa">Visa</option>
+      <option value="MasterdCard">MasterCard</option>
+      <option value="GiftCard">GiftCard</option>
+    </select>
+    <p>Payment: {payment}</p>
   </div>)
+
+  
 }
 
   
