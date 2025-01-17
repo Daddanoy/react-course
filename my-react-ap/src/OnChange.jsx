71cf5130afd512c0a36a8391d066df01a9941ddf
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-17 11:24:49 +0000

    ONCHANGE TEXTAREA/COMENT

--- my-react-ap/src/OnChange.jsx
+++ my-react-ap/src/OnChange.jsx
@@ -3,6 +3,12 @@ import React, {useState} from 'react'
 function OnChange (){
 
   const [name, setName] = useState("");
+  const [qunatity, setQuantity] = useState();
+
+  function handleQuantityChange(event){
+    setQuantity(event.target.value)
+    
+  }
 
   function handleChange(event){
     setName(event.target.value)
@@ -12,6 +18,9 @@ function OnChange (){
   return(<div>
     <input value ={name} onChange={handleChange} />
     <p>Name: {name}</p>
+
+    <input value = {qunatity} onChange={handleQuantityChange} type='number' />
+    <p> Qunatity: {qunatity}</p>
   </div>)
 }
 
