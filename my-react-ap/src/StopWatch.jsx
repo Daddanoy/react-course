360857299d896b279e5d1463d976d9160c251c3d
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-21 22:45:39 +0000

    finished stopwatch app

--- my-react-ap/src/StopWatch.jsx
+++ my-react-ap/src/StopWatch.jsx
@@ -10,23 +10,43 @@ function StopWatch(){
   const startTimeRef = useRef(0) //when we start our watch we need to get the current time
 
   useEffect(()=>{
+    if(isRunning){
+      intervalIdRef.current = setInterval(()=>{
+     setElapsedTime(Date.now()-startTimeRef.current)
+      },10)
+
+      return ()=>{
+        clearInterval(intervalIdRef.current)
+      }
+
+    }
 
   },[isRunning]);
 
   function start(){
-
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
+    console.log(startTimeRef.current)//converts date and time into ms since the 1970's
   }
 
   function stop(){
-
+    setIsRunning(false)
   }
 
   function reset(){
-
+    setElapsedTime(0)
+    setIsRunning(false)
   }
 
   function formatTime(){
-    return`00:00:00`
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60))
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60)
+    let seconds = Math.floor(elapsedTime / (1000) % 60)
+    let milliseconds = Math.floor(elapsedTime % 1000/10)
+
+
+    return`${minutes}:${seconds}:${milliseconds} `
 
   }
 return (<>
