ddcf953b03f1e3f7a8a930268f8e1e577bd903d4
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-21 09:34:23 +0000

    USECONTEXT. STRARTEDD OFF BY USING PROPS DRILLING

--- my-react-ap/src/App.jsx
+++ my-react-ap/src/App.jsx
@@ -1,5 +1,5 @@
 
-import DigitalClock from "./Digital Clock"
+import ComponentA from "./ComponentA"
 
 
 
@@ -9,7 +9,7 @@ function App() {
 
 return(<>
 
-<DigitalClock />
+<ComponentA />
 
 </>)
 }
