8fd2e2d6b9855ed7661910537d64efb93f84c56b
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-21 20:10:31 +0000

    Added useref focus and updated code

--- my-react-ap/src/UseRef.jsx
+++ my-react-ap/src/UseRef.jsx
@@ -2,23 +2,30 @@ import React, {useState, useEffect, useRef} from 'react'
 
 function MyComponent(){
 
-let [number, setNumber] = useState(0)
+//let [number, setNumber] = useState(0)
+const inputRef = useRef(null)
 
 useEffect(()=>{
   console.log('COMPONENT RENDERED')
+  console.log(inputRef)
 })
 
 function handleClick(){
-  setNumber(n => n+1)
+  //setNumber(n => n+1)
+
+inputRef.current.focus()
+inputRef.current.style.backgroundColor = 'yellow'
+  
 
 }
 
-  return( <>
+  return( <div>
   <button onClick={handleClick}>
     Click Me
   </button>
-  <p>{number}</p>
-  </>)
+  <input ref={inputRef} />
+  
+  </div>)
   }
   
   export default MyComponent
