91742afe595f09940543feea6ff6bf80c739e68b
yonnik <137620324+yonnik@users.noreply.github.com>
2025-01-23 09:43:50 +0000

    DYNAMIC STYLING FINISHED

--- my-react-ap/src/React Projects/LightDarkMode.jsx
+++ my-react-ap/src/React Projects/LightDarkMode.jsx
@@ -2,7 +2,31 @@ import React, {useState} from 'react'
 
 function DynamicStyling(){
 
+  const[darkMode, setDarkMode] = useState(false);
+
+  function darkPallette(){
+    setDarkMode(true)
+   
+
+  }
+
+  function lightPallete(){
+    setDarkMode(false)
+
+   
+  }
+
+
   
+
+  return(
+    <div className={darkMode? 'dark-background' : 'light-background'}>
+      <h1 className ="style-header">Dynamic Styling</h1>
+
+  
+      <button onClick={darkMode? lightPallete : darkPallette} className={darkMode? 'light-mode-button' : 'dark-mode-button'}>{darkMode? 'Lightmode' : 'Dark Mode'}</button>
+    </div>
+  )
 }
 
 export default DynamicStyling
